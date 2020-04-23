9f5c3328b1b3387d5cd54b01633cfa5ca7f497f0
timokie <xiaoshame@126.com>
2020-04-23 15:06:22 +0800

    添加测试

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,21 +1,9 @@
 import  React, {FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
 import classnames from 'classnames';
 
-
-
-
 export type ButtonSize = 'lg' | 'sm';
 export type ButtonType = 'primary' | 'default' | 'danger' | 'link';
-// export enum ButtonSize{
-//     Large = 'lg',
-//     Small = 'sm'
-// }
-// export enum ButtonType {
-//     Primary = 'primary',
-//     Default = 'default',
-//     Danger = 'danger',
-//     Link = 'link'
-// }
+
 interface BaseButtonProps {
     size?: ButtonSize;
     btnType?: ButtonType;
